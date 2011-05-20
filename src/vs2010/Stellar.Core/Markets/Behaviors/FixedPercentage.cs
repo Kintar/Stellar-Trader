1f9a49032159ee5529be5b17ddea578d44e54507
Alec Lanter <kintar1900@gmail.com>
2011-05-20 15:30:04 -0500

    Beginning work on market pricing

---
+++ src/vs2010/Stellar.Core/Markets/Behaviors/FixedPercentage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stellar.Core.Items;
+
+namespace Stellar.Core.Markets.Behaviors
+{
+    public class FixedPercentage : IMarketBehavior
+    {
+        public double BuyMultiplier { get; protected set; }
+        public double SellMultiplier { get; protected set; }
+
+        public string Name
+        {
+            get { return "Fixed Markup"; }
+        }
+
+        public double CalculateSellPrice(Item item, int quantityOnHand, int orderQuantity)
+        {
+            return (item.BaseValue * SellMultiplier) * orderQuantity;
+        }
+
+        public double CalculateBuyPrice(Item item, int quantityOnHand, int orderQuantity)
+        {
+            return (item.BaseValue * BuyMultiplier) * orderQuantity;
+        }
+    }
+}
---
+++ src/vs2010/Stellar.Tests/Markets/MarketPriceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Stellar.Core.Markets;
+using Stellar.Core.Items;
+using Stellar.Core.Markets.Behaviors;
+
+namespace Stellar.Tests.Markets
+{
+    [TestFixture]
+    public class MarketPriceTests
+    {
+        [Test]
+        public void TestFixedPercentageBehavior()
+        {
+            Item grain = new Item("Grain", 1, 1, 1);
+            Crate grainCrate = new Crate(grain, 50);
+            List<Crate> inventory = new List<Crate>();
+            inventory.Add(grainCrate);
+            Market m = new Market("Grainplace, Inc.", inventory);
+            m.Behaviors[grain] = new FixedPercentage();
+        }
+    }
+}
