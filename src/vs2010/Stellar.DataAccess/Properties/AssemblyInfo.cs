c873577f3966d546f07f9e852dbe2c15dcde165b
Alec Lanter <kintar1900@gmail.com>
2011-05-30 11:55:50 -0500

    Removing distracting clutter

--- src/vs2010/Stellar.DataAccess/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0526ed57-62c3-46b1-8270-e90e60933322")]
--- src/vs2010/Stellar.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("504c16e7-2cd8-4f05-919f-eec50121f7c6")]
