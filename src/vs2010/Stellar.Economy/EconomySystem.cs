170aa8b4a63a929207bf5b90b1c3ad162e502e06
Alec Lanter <kintar1900@gmail.com>
2011-05-30 11:54:43 -0500

    Added EconomySystem project

---
+++ src/vs2010/Stellar.Economy/EconomySystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InfinitySystem;
+
+namespace Stellar.EconomySystem
+{
+    public class EconomySystem : SubSystem
+    {
+        public override void Tick(long millisecondsSinceLastTick)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
